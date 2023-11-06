7cac4489edf7b79d923d60c6c920d754d162f471
joker7127 <tritran.dtu.94@gmail.com>
2023-11-06 20:56:54 +0700

    new

---
+++ getting-JS/script.js
@@ -0,0 +1,27 @@
+console.log("external HTML")
+let a = 6; // gán giá trị 6 cho a
+let b = 10; // gán giá trị 10 cho b
+
+let c = a + b; // lấy a+b sau gán giá trị cho c
+console.log("tổng là:", c);
+
+
+
+
+
+// Inc consple
+// console.log "TỔNG LÀ:", C);
+
+//  Hàm ALERT()
+//alert("tổng là:" + c);
+
+// hàm prompt(): dùng để nhập dữ liệu.
+
+//prompt("nhập số:"); 
+
+
+//hàm confirm(): thông báo xác nhận 1 vấn đề
+//confirm("bạn có mướn nhập số này ko:");
+
+// in trên trang web: document.write();
+//document.write("tổng
